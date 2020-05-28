5ac204d643eff38153f4f9a2595767355b5c485e
dork7 <hamzameh122@gmail.com>
2020-05-28 19:21:15 +0500

    day 2

--- index.js
+++ index.js
@@ -1,13 +1,39 @@
-const express = require('express');
-const mongoose = require('mongoose');
+const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
 
-mongoose.connect('mongodb+srv://dork7:hitler564@cluster0-7kxtv.mongodb.net/test?retryWrites=true&w=majority', 
-{ useNewUrlParser :true , useUnifiedTopology: true })
-.then( () => console.log("DB Connected")).catch(err => console.error(err))
+const { User } = require("./model/user");
 
-app.get('/', function (req, res) {
-    res.send('hello world')
+mongoose
+  .connect(
+    "mongodb+srv://dork7:hitler564@cluster0-7kxtv.mongodb.net/test?retryWrites=true&w=majority",
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then(() => console.log("DB Connected"))
+  .catch((err) => console.error(err));
+
+app.get("/", function (req, res) {
+  res.send("hello world");
+});
+
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
   })
+);
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+app.post("/api/users/register", (req, res) => {
+  const user = new User(req.body);
+  user.save((err, userData) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({
+      success: true,
+    });
+  });
+});
 
 app.listen(5000);
